0d6af1db4fe7dd28577a8c12c7250993024efe4c
nicolasarruda <nlarruda@hotmail.com>
2022-08-16 19:00:54 -0300

    First commit

---
+++ frontweb/src/pages/Admin/index.tsx
@@ -0,0 +1,27 @@
+import Navbar from './Navbar';
+
+import './styles.css';
+import { Route, Switch } from 'react-router-dom';
+
+const Admin = () => {
+  return (
+    <div className="admin-container">
+        <Navbar />
+        <div className="admin-content">
+        <Switch>
+          <Route path="/admin/products">
+            <h1>Product CRUD</h1>
+          </Route>
+          <Route path="/admin/categories">
+            <h1>Category CRUD</h1>
+          </Route>
+          <Route path="/admin/users">
+            <h1>User CRUD</h1>
+          </Route>
+        </Switch>
+        </div>
+    </div>
+  );
+};
+
+export default Admin;
