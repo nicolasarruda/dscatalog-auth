c893af03d02b4843904d209ece112e12e686c900
nicolasarruda <nlarruda@hotmail.com>
2022-08-20 20:39:18 -0300

    Check user authenticated

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -3,10 +3,12 @@ import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
+import { isAuthenticated } from '../../util/requests';
 
 const Home = () => {
   return (
     <div className="home-container">
+      <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
@@ -18,7 +20,7 @@ const Home = () => {
           </div>
           <div>
             <Link to="/products">
-              <ButtonIcon text="Inicie agora a sua busca"/>
+              <ButtonIcon text="Inicie agora a sua busca" />
             </Link>
           </div>
         </div>
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,15 @@
 import qs from 'qs';
 import axios, { AxiosRequestConfig } from 'axios';
 import history from './history';
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+  exp: number;
+  user_name: string;
+  authorities: Role[];
+};
 
 type LoginResponse = {
   access_token: string;
@@ -92,3 +101,17 @@ axios.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+export const getTokenData = (): TokenData | undefined => {
+  try {
+    return jwtDecode(getAuthData().access_token) as TokenData;
+  } catch (error) {
+    return undefined;
+  }
+};
+
+export const isAuthenticated = (): boolean => {
+  const tokenData = getTokenData();
+
+  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
+};
