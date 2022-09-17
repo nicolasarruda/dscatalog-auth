be0d00b4126846f400d32952a75d26bd2aa9e303
nicolasarruda <nlarruda@hotmail.com>
2022-09-16 21:59:37 -0300

    Select React Hook Form integration

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
 import { Category } from 'types/category';
@@ -26,6 +26,7 @@ const Form = () => {
     handleSubmit,
     formState: { errors },
     setValue,
+    control,
   } = useForm<Product>();
 
   useEffect(() => {
@@ -101,13 +102,28 @@ const Form = () => {
 
               <div className="margin-bottom-30">
                 <div className="mb-4">
-                  <Select
-                    options={selectCategories}
-                    classNamePrefix="product-crud-select"
-                    isMulti
-                    getOptionLabel={(category: Category) => category.name}
-                    getOptionValue={(category: Category) => String(category.id)}
+                  <Controller
+                    name="categories"
+                    rules={{ required: true }}
+                    control={control}
+                    render={({ field }) => (
+                      <Select
+                        {...field}
+                        options={selectCategories}
+                        classNamePrefix="product-crud-select"
+                        isMulti
+                        getOptionLabel={(category: Category) => category.name}
+                        getOptionValue={(category: Category) =>
+                          String(category.id)
+                        }
+                      />
+                    )}
                   />
+                  {errors.categories && (
+                    <div className="invalid-feedback d-block">
+                      Campo obrigatório
+                    </div>
+                  )}
                 </div>
               </div>
 
